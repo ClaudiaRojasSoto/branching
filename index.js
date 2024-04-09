586bc413ede0038aa3f8da932df183a0bbd6a1da
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-04-09 00:43:16 -0400

    Add an on click function to load repos id to change the visualitation to block instead none, to deploy a list of repositories called using Ajax and jquery, including a target blanc for each repo on the list and a tooltip with the 'clickeame' message

--- index.js
+++ index.js
@@ -4,5 +4,23 @@ $(document).ready(function() {
       $("#staticBackdropLabel").toggleClass("light-theme");
       $('#modal-content-full').toggleClass('light-theme');
     });
+  
+    $('#load-repos').click(function() {
+        $('#repos-list').css('display', 'block');
+      $.ajax({
+        url: 'https://api.github.com/users/ClaudiaRojasSoto/repos',
+        success: function(repos) {
+            let reposList = $('#repos-list');
+            reposList.empty();
+            repos.forEach(function(repo) {
+              reposList.append('<p><a href="' + repo.html_url + '" target="_blank" data-bs-toggle="tooltip" title="¡Clíckame!">' + repo.name + '</a></p>');
+            });
+            $('[data-bs-toggle="tooltip"]').tooltip();
+          },
+        error: function() {
+          $('#repos-list').append('<p>Se produjo un error al cargar los repositorios.</p>');
+        }
+      });
+    });
   });
   
