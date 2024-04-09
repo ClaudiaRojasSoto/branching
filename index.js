52a8b19cc46f4cf2751921e517067e5be9e80194
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-04-08 23:36:41 -0400

    Add the on click function using jquery to use the theme toggler to change to use the light theme when press the theme toggler button

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function() {
+    $('#theme-toggler').click(function() {
+      $('#staticBackdrop').toggleClass('light-theme');
+      $("#staticBackdropLabel").toggleClass("light-theme");
+      $('#modal-content-full').toggleClass('light-theme');
+    });
+  });
+  
