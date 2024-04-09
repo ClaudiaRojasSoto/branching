7ced8bf4c56bccd35d2ed99b41b3a5a24fa31e6a
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-04-08 22:02:04 -0400

    Delete the code to add a blur class

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-    $('#btn-modal').on('click', function() {
-        $('.container').addClass('blur');
-    });
 
-    $('.btn-close, .btn-secondary').on('click', function() {
-        $('.container').removeClass('blur');
-    });
-});
